f8d3315363005457512a28a4dc98ada3eef84152
theHeliE <129796088+theHeliE@users.noreply.github.com>
2023-12-24 22:02:30 +0200

    Update Sponsors.tsx

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,30 +1,46 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Toolbar from "./Toolbar";
 import { Carousel } from "react-bootstrap";
 import { Card, Container, Row, Col } from "react-bootstrap";
 import Footer from "./Footer";
-import magnusinc from "../images/magnusinc.jpg";
-const data = [
-  {
-    id: 1,
-    title: "57357",
-    content: "https://cdn2.downdetector.com/static/uploads/logo/3_JNoDoWg.png",
-  },
-  {
-    id: 2,
-    title: "MAGNUS INC.",
-    content: magnusinc,
-  },
-  {
-    id: 3,
-    title: "Shell",
-    content:
-      "https://cdn.icon-icons.com/icons2/2699/PNG/512/shell_logo_icon_169759.png",
-  },
-];
+//import magnusinc from "../images/magnusinc.jpg";
+type sponsorsData = {
+  nameS: string;
+  ContactNumber: number;
+  CarID: number;
+  Logo: string;
+};
 
+// const data = [
+//   {
+//     id: 1,
+//     title: "57357",
+//     content: "https://cdn2.downdetector.com/static/uploads/logo/3_JNoDoWg.png",
+//   },
+//   {
+//     id: 2,
+//     title: "MAGNUS INC.",
+//     content: magnusinc,
+//   },
+//   {
+//     id: 3,
+//     title: "Shell",
+//     content:
+//       "https://cdn.icon-icons.com/icons2/2699/PNG/512/shell_logo_icon_169759.png",
+//   },
+// ];
 export default function Sponsors() {
+  const [data, setData] = useState<sponsorsData[]>([]);
+  useEffect(() => {
+    fetch("http://localhost:8081/sponsors/")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  });
   return (
     <div className="general-appearance">
       <Toolbar />
@@ -34,11 +50,11 @@ export default function Sponsors() {
           style={{ width: "600px", margin: "auto" }}
         >
           {data.map((item) => (
-            <Carousel.Item key={item.id}>
-              <h3>{item.title}</h3>
+            <Carousel.Item key={item.CarID}>
+              <h3>{item.nameS}</h3>
               <img
                 style={{ width: "100%", height: "300px" }}
-                src={item.content}
+                src={item.Logo}
                 alt=""
               />
             </Carousel.Item>
